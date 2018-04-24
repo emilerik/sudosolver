686d249e462fee0acbd326063f95b7ce100e28af
Algirdas Bartkevicius <algba880@student.liu.se>
2018-04-24 09:50:31 +0200

    Created new branch

--- main.rkt
+++ main.rkt
@@ -1,6 +1,3 @@
 #lang racket
 (require "world_init.rkt")
 (require "skiss.rkt")
-
-(define x
-  (+ 1 2))
--- world_init.rkt
+++ world_init.rkt
@@ -38,5 +38,23 @@
        [row-number 9]))
 
 
-       
+(define col2
+  (new column%
+       [column-number 2]))
+
+(define col5
+  (new column%
+       [column-number 9]))
+
+(hash-set! *elements* 1 (send row1 get-element column-number))
+(hash-set! *elements* 2 (send row2 get-element column-number))
+(hash-set! *elements* 3 (send row3 get-element column-number))
+(hash-set! *elements* 4 (send row4 get-element column-number))
+(hash-set! *elements* 5 (send row5 get-element column-number))
+(hash-set! *elements* 6 (send row6 get-element column-number))
+(hash-set! *elements* 7 (send row7 get-element column-number))
+(hash-set! *elements* 8 (send row8 get-element column-number))
+(hash-set! *elements* 9 (send row9 get-element column-number))
+
+
        
