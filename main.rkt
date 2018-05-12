dac133584e43e1827b468b0c198b1612e6a21530
Emil <emier834@student.liu.se>
2018-05-12 21:29:10 +0200

    trying something

--- main.rkt
+++ main.rkt
@@ -33,5 +33,39 @@
    (range 0 81))
   (initialize-candidates! board))
 
-         
+(define (clear-filled-elems! board) ;; Clears all values that aren't flagged as user-values
+  (cond
+    [(null? board)
+     (void)]
+    [(send (car board) user-val?)
+     (clear-filled-elems! (cdr board))]
+    [else
+     (send (car board) set-value! 0)
+     (send (car board) reset-all-candidates!)
+     (clear-filled-elems! (cdr board))]))
+
+(define (step-solve! board)
+  (unless (sudoku-solved?) (solve-sudoku! board))
+  (let ((rnd-elem (list-ref board (random 0 81))))
+    (cond
+      [(send rnd-elem user-val?)
+       (step-solve! board)]
+      [else
+       (send rnd-elem set-user-e! #t)
+       (clear-filled-elems! board)
+       (initialize-candidates! board)])))
+
+(define (solve-sudoku! board)
+  (cond
+    [(sudoku-solved?) (printf "Sudoku already solved!")] ;; Only run function if sudoku not yet solved
+    [(not (sudoku-solvable?))
+     (printf "Error: sudoku cannot be solved - duplicate elements in row, column and/or box.")
+     (clear-filled-elems! board)]
+    [else
+     (solving-algorithm board)]))
+       
 (set-board! brd sdk1)
+;(set-board! brd false-sdk1)
+
+(define rezz 0)
+(set! rezz brd)
--- solver_functions.rkt
+++ solver_functions.rkt
@@ -3,8 +3,32 @@
 (require "classes.rkt")
 (require "sudoku_init.rkt")
 
+(define (valid-holder? holder) ;; decides if a holder contains any multiple elements or illegal elements (> 9 or < 0)
+  (let ([holder-vals (map (lambda (e) (send e get-value))
+                          holder)])
+    (define (helper vals)
+      (cond
+        [(null? vals) #t]
+        [(= 0 (car vals)) (helper (cdr vals))]
+        [(or (> (car vals) 9) (< (car vals) 0) (member (car vals) (cdr vals))) #f]
+        [else (helper (cdr vals))]))
+    (helper holder-vals)))
 
-(define (correct-holder? holder) ;; decides if a holder (row, column or box) contains the correct values (1-9 exactly once)
+(define (sudoku-solvable?) ;; decides if sudoku has illegal (multiple) elements in a row, col and/or box
+  (define (helper rest-of-holders)
+    (cond
+      [(null? rest-of-holders) #t]
+      [(valid-holder? (car rest-of-holders)) (helper (cdr rest-of-holders))]
+      [else #f]))
+  (helper holders))
+       
+      
+        
+
+;; row1 = list of elements
+;; rows = list of rows
+
+(define (solved-holder? holder) ;; decides if a holder (row, column or box) contains the correct values (1-9 exactly once)
   (define (helper vals)
     (cond
       [(null? vals) #t]
@@ -12,62 +36,76 @@
       [else (helper (cdr vals))]))
   (helper (range 1 10)))
 
-;; row1 = list of elements
-;; rows = list of rows
+;(define (multiple-solutions? board
 
-(define (sudoku-solved?) ;; this function has three components: a "holder" inside "holders", for example row1 inside rows
+(define (sudoku-solved?) ;; returns #t if all holders are solved, and if there is a unique solution (UNIQUE CHECK DOESN'T WORK YET)
   (not (member #f (flatten
                    (map ;; this map gets rows, cols, boxes
                     (lambda (holders)
                       (map ;; this map gets row1, row2, row3..., col1, col2, col3..., box1, box2, box3...
                        (lambda (holder)
-                         (correct-holder? (get-values holder))) ; checks if for example row1 contains correct elements
+                         (solved-holder? (get-values holder))) ; checks if for example row1 contains correct elements
                        holders))
                     (list rows cols boxes))))))
+           
 
 (define i 1)
 (define j 0)
+(define iter 0)
 
+(define (solving-algorithm board)
+  (let ([first-e (car board)] [solved-once #f] [result '()] [i 1] [j 0] [iter 0])
+    (define (helper prev-e curr-e next-e f) ;; f indicates if the algorithm is going backward or forward. f = #t: forward, f = #f: backward
+      (set! j (+ j 1)) ;; iteration counter
+      (cond
+        [(equal? (send curr-e get-value) 'last) ;; Made it to the last element
+         (cond
+           [solved-once
+            (printf "Error: Multiple solutions found")]
+           [else (set! solved-once #t)
+                 (set! result brd)
+                 (set! iter j)
+                 (set! i (- i 1))
+                 (helper (send prev-e get-pr-e) prev-e curr-e #f)])]
 
-(define (solve-sudoku board)
-  (if (sudoku-solved?)
-      (printf "Sudoku already solved!") ;; Only run function if sudoku not yet solved!
-      (let ((first-ele (car board)))
-        (define (helper prev-e curr-e next-e f) ;; f indicates if the algorithm is going backward or forward. f = #t: forward, f = #f: backward
-          (set! j (+ j 1)) ;; iteration counter
-          (cond
-            [(equal? (send curr-e get-nx-e) "") ;; Made it to the last element
-             (printf "Sudoku solved! Number of iterations: ~a ~n ~n" j)
-             (print-board)]
-
-            [(send curr-e user-val?)
-             (cond
-               [(or (eqv? prev-e "") f) 
-                (send next-e update-candidates! #t)
-                ;(printf "User-cell ~a. Going forward ~n" i) ;; comment for debugging
-                (helper curr-e next-e (send next-e get-nx-e) #t)]
-               [else
-                ;(printf "User-cell ~a. Going backward ~n" i)
-                (helper (send prev-e get-pr-e) prev-e curr-e #f)])]
+        [(send curr-e user-val?)
+         (cond
+           [(and solved-once (equal? 'first prev-e))
+            (printf "Sudoku solved! Number of iterations: ~a ~n" iter)
+            (print-board result)]
+           [(or (eqv? prev-e 'first) f)
+            (send next-e update-candidates! #t)
+            ;(when (< i 10) (printf "User-cell ~a. Going forward ~n" i)) (set! i (+ i 1)) ;; comment for debugging
+            (helper curr-e next-e (send next-e get-nx-e) #t)]
+           [else
+            ;(when (< i 10) (printf "User-cell ~a. Going backward ~n" i)) (set! i (- i 1))
+            (helper (send prev-e get-pr-e) prev-e curr-e #f)])]
         
-            [(send curr-e empty-cand?)
-             ;(printf "Cell ~a. No candidates. Resetting candidates. Going backwards ~n" i)
+        [(send curr-e empty-cand?)
+         (cond
+           [(and solved-once (equal? 'first prev-e)) ;; Unique solution!
+            (printf "Sudoku solved! Number of iterations: ~a ~n" iter)
+            (print-board result)]
+           [else
+             ;(when (< i 10) (printf "Cell ~a. No candidates. Resetting candidates. Going backwards ~n" i)) (set! i (- i 1))
              (send curr-e set-value! 0)
              (send curr-e reset-candidates!)
-             (helper (send prev-e get-pr-e) prev-e curr-e #f)]
+             (helper (send prev-e get-pr-e) prev-e curr-e #f)])]
         
-            [else
-             ;(printf "Cell ~a. Candidates: ~a. " i (send curr-e get-candidates))
-             (send curr-e set-cand-to-val!)
-             (send next-e update-candidates! #t)
-             ;(printf "Set value to ~a. Next element candidates ~a. Going forward. ~n" (send curr-e get-value) (send next-e get-candidates))
-             (helper curr-e next-e (send next-e get-nx-e) #t)]))
-        (helper '() first-ele (send first-ele get-nx-e) #t))))
+        [else
+         ;(when (< i 10) (printf "Cell ~a. Candidates: ~a. " i (send curr-e get-candidates)))
+         (send curr-e set-cand-to-val!)
+         (send next-e update-candidates! #t)
+         ;(when (< i 10) (printf "Set value to ~a. Next element candidates ~a. Going forward. ~n" (send curr-e get-value) (send next-e get-candidates))) (set! i (+ i 1))
+         (helper curr-e next-e (send next-e get-nx-e) #t)]))
+    (helper 'first first-e (send first-e get-nx-e) #t)))
 
-(define (print-board) ;; prints sudoku board
+
+
+(define (print-board board) ;; prints sudoku board
   (for-each
    (lambda (i)
-     (send (list-ref brd i) print-value)
+     (send (list-ref board i) print-value)
      (when (= (remainder (+ i 1) 9) 0)
        (newline)))
    (range 0 81)))
